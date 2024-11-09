3decaa2460a596ea61bd6c865bf675df5ac08200
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2024-11-09 12:13:31 -0500

    Fixes KeyError

--- python/client.py
+++ python/client.py
@@ -12,19 +12,45 @@ else:
     import SocketServer as ss
 
 
+# class NetworkHandler(ss.StreamRequestHandler):
+#     def handle(self):
+#         game = Game()
+
+#         while True:
+#             data = self.rfile.readline().decode() # reads until '\n' encountered
+#             json_data = json.loads(str(data))
+#             # uncomment the following line to see pretty-printed data
+#             print(json.dumps(json_data, indent=4, sort_keys=True))
+#             response = game.get_random_move(json_data).encode()
+#             self.wfile.write(response)
+
 class NetworkHandler(ss.StreamRequestHandler):
     def handle(self):
         game = Game()
 
+    
+
+        count = 0
+
         while True:
-            data = self.rfile.readline().decode() # reads until '\n' encountered
-            json_data = json.loads(str(data))
-            # uncomment the following line to see pretty-printed data
-            print(json.dumps(json_data, indent=4, sort_keys=True))
+
+            data = self.rfile.readline().decode()  # reads until '\n' encountered
+            if not data.strip():  # skip empty lines
+                continue
+            try:
+                json_data = json.loads(str(data))
+            except Exception as e:
+                print("Unexpected error:", e)
+
+            if count == 0:
+                game.initialize_map(json_data)
+                    
+
+        
             response = game.get_random_move(json_data).encode()
             self.wfile.write(response)
 
-
+            count += 1        
 
 class Game:
     def __init__(self):
@@ -32,14 +58,11 @@ class Game:
         self.directions = ['N', 'S', 'E', 'W']
 
 
-    def update_tiles(self, json_data, previous_map_state):
-        
-
-        return
-
-
     def get_map_size(self, json_data):
-        return (json_data["game_info"]["map_width"], json_data["game_info"]["map_height"])
+        width = json_data["game_info"]["map_width"]
+        height = json_data["game_info"]["map_height"]
+
+        return (width, height)
 
     def initialize_map(self, json_data):
         map_dimensions = self.get_map_size(json_data)
@@ -48,10 +71,11 @@ class Game:
         two_dee_map = ([0] * map_dimensions[0])
         two_dee_map = [[0]*map_dimensions[0]]*map_dimensions[1]
         print(two_dee_map) # Print 2D Array
-        
 
+        return
 
     def get_random_move(self, json_data):
+        #two_dee_map = self.initialize_map(json_data)
         units = set([unit['id'] for unit in json_data['unit_updates'] if unit['type'] != 'base'])
         self.units |= units # add any additional ids we encounter
         unit = random.choice(tuple(self.units))
@@ -60,8 +84,7 @@ class Game:
         command = {"commands": [{"command": move, "unit": unit, "dir": direction}]}
         response = json.dumps(command, separators=(',',':')) + '\n'
         print(json_data["unit_updates"])
-        self.initialize_map(json_data)
-
+        #self.update_tiles(json_data, two_dee_map)
 
         return response
 
